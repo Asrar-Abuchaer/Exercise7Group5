faac62a37dedc5ba756ecee2757663b012b093fe
I Putu Phillip Steven <putu.phillip@gmail.com>
2023-10-02 16:04:31 +0700

    Update Soal4

---
+++ group5/src/Pages/Soal4/Pages/Chat/index.jsx
@@ -0,0 +1,9 @@
+function Chat() {
+  return (
+    <div className="container">
+      <div className="containerChild header">
+        <h1>Chat</h1>
+      </div>
+    </div>
+  );
+}
--- group5/src/Pages/Soal4/index.jsx
+++ group5/src/Pages/Soal4/index.jsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom";
 function Soal4() {
   return (
-    <div className="container">
+    <div className="container" id="Soal4">
       <div className="containerChild header">
         <h2>Soal 4</h2>
+        <h3>ChitChat - Express Your Feeling</h3>
+      </div>
+      <div className="containerChild body">
+        <textarea name="" id="chatInput"></textarea>
+        <button>Send</button>
+      </div>
+      <div className="containerChild footer">
+        <Link id="browserLink" to="/">
+          Home
+        </Link>
       </div>
     </div>
   );
