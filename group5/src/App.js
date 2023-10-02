1a6f7489da4d92aca1058a1153a81d7fedd05aad
I Putu Phillip Steven <putu.phillip@gmail.com>
2023-10-02 22:30:23 +0700

    Update Soal 4

--- group5/src/App.js
+++ group5/src/App.js
@@ -5,18 +5,23 @@ import { Routes, Route } from "react-router-dom";
 import Soal1 from "./Pages/Soal1";
 import Soal2 from "./Pages/Soal2";
 import Soal3 from "./Pages/Soal3";
-import Soal4 from "./Pages/Soal4";
+import { Context, Soal4 } from "./Pages/Soal4";
+import { ContextProvider } from "./Pages/Soal4/Pages/Context";
+import Chat from "./Pages/Soal4/Pages/Chat";
 function App() {
   return (
-    <div className="App" id="MainApp">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route id="browserRoute" path="soal1" element={<Soal1 />} />
-        <Route id="browserRoute" path="soal2" element={<Soal2 />} />
-        <Route id="browserRoute" path="soal3" element={<Soal3 />} />
-        <Route id="browserRoute" path="soal4" element={<Soal4 />} />
-      </Routes>
-    </div>
+    <ContextProvider>
+      <div className="App" id="MainApp">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route id="browserRoute" path="soal1" element={<Soal1 />} />
+          <Route id="browserRoute" path="soal2" element={<Soal2 />} />
+          <Route id="browserRoute" path="soal3" element={<Soal3 />} />
+          <Route id="browserRoute" path="soal4" element={<Soal4 />} />
+          <Route id="browserRoute" path="soal4/chat" element={<Chat />} />
+        </Routes>
+      </div>
+    </ContextProvider>
   );
 }
 
--- group5/src/Pages/Soal4/Pages/Chat/index.jsx
+++ group5/src/Pages/Soal4/Pages/Chat/index.jsx
@@ -1,9 +1,20 @@
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Context } from "../Context";
+
 function Chat() {
+  const navigate = useNavigate();
+  const { items, setItems } = useContext(Context);
   return (
     <div className="container">
-      <div className="containerChild header">
-        <h1>Chat</h1>
+      <div className="containerChild">{items}</div>
+      <div className="containerChild">
+        <Link id="browserLink" to="/">
+          Home
+        </Link>
       </div>
     </div>
   );
 }
+
+export default Chat;
