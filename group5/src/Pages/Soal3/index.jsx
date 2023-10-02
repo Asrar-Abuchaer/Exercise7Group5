1d1c83caf7248df983339ddb7507ea17b440e1fd
I Putu Phillip Steven <putu.phillip@gmail.com>
2023-10-02 11:24:51 +0700

    Update index.js

--- group5/src/Pages/Soal3/index.jsx
+++ group5/src/Pages/Soal3/index.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 function Soal3() {
   return (
     <div className="container">
-      <div className="containerChild header">
+      <div className="containerChild soal3Header">
         <h2>Soal 3</h2>
       </div>
-      <div className="containerChild body">
+      <div className="containerChild soal3Body">
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci
           reiciendis itaque molestiae harum, alias enim expedita vel sit dolor
